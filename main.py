1641382b21d9e80c9abd837f0ddd964bd1b0cabc
gig121212 <sunyeongbang98@gmail.com>
2023-06-07 08:09:11 +0900

    update while board and checking insert on board

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from tic_tac_toe import TicTacToe
 
+#setting game
 board_game_is_on = False
+
 
 
 tictactoe = TicTacToe()
 
+
+
 board = tictactoe.makingANewBoard(board_num=3)
 
+#after setting board board_game_is_on set True
 
 board_game_is_on = True
-print(board)
-
 
 
 user_input = input("Please enter first player's position. O or X ? : ")
 
-player_one = tictactoe.makingUpperCase(userInput=user_input)
 
+player_one = tictactoe.makingUpperCase(userInput=user_input)
 player_two = tictactoe.setOtherPlayer(first_player_input=player_one)
 
-print(f"first player : {player_one} \n other player: {player_two}")
+print(f"first player : {player_one} \nother player: {player_two}")
 
 play_times = 1
 
@@ -31,9 +34,9 @@ while board_game_is_on:
        player_side = player_two
 
 
-    player_input = input("Please input your choice as [row][column] like 11 or 12 : ")
+    print(board)
 
-    print(player_input)
+    player_input = input("Please input your choice as [row][column] like 11 or 12 : ")
 
     checkUserInput = tictactoe.checking_user_input(playerInputs=player_input)
 
@@ -43,14 +46,15 @@ while board_game_is_on:
     checkBoard = tictactoe.checkingIfBoardFilled(board=board , row=int(player_input[0]) ,column=int(player_input[1]))
 
     if not checkBoard :
-        player_input = input("that place already be chosen.Please choose empty space")
+        player_input = input("that place already be chosen. Please choose empty space : ")
 
+    print(checkBoard)
     #player_input = tictactoe.player_input(playerInput=player_input)
 
-    board_list = tictactoe.inserting_board(board_list=board, player_side=player_side , row=int(player_input[0]) , column=int(player_input[1]) )
+    board = tictactoe.inserting_board(board_list=board, player_side=player_side , row=int(player_input[0]) , column=int(player_input[1]) )
 
-
-    print(board_list)
+    output_result = "After input Board : {}".format(board)
+    print(output_result)
 
     play_times += 1
 
