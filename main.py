2e7064ac67102b79ba23b4c241247cb24dd3ad3e
gig121212 <sunyeongbang98@gmail.com>
2023-09-01 08:00:33 +0900

    add while loop

--- main.py
+++ main.py
@@ -41,69 +41,68 @@ def play_tic_tac_toe():
     player_one = validate_result
     player_two = tictactoe.set_other_player(first_player_input=player_one)
     print(f"first player : {player_one} \nother player: {player_two}")
-
-    #couting the number to find which user playing
-    turn_count = 1
-
-
-
-    #checking user_position with turn_count
-    if turn_count % 2 != 0 :
-       player_side = player_one
-    else:
-       player_side = player_two
-
-
     #board printing
     print(board)
+    turn_count = 1
+    while board_game_is_on:
 
-    while True:
-        #change number to start with 1 not 0
-        player_input = input("Please enter your choice from 1 to 9 : ")
-        if tictactoe.validate_user_input(player_input=player_input):
-            break
+        # couting the number to find which user playing
+
+
+        # checking user_position with turn_count
+        if turn_count % 2 != 0:
+            player_side = player_one
         else:
-            print("Invalid input , please try again")
-
-    player_input = tictactoe.player_input(player_inputs = player_input)
-
-    check_the_board = tictactoe.check_whole_board(board=board , row=int(player_input[0]) ,column=int(player_input[1]))
-
-    if not check_the_board :
-        player_input = input("that place already be chosen. Please choose empty space : ")
-
-    print(check_the_board)
-
-    board = tictactoe.inserting_board(board_list=board, player_side=player_side , input = player_input)
-    display_board = tictactoe.display_board_after_insert(board_list=board)
-
-    print(display_board)
-
-    # check if turn count is over five and under nine then check
-    if turn_count >= 5:
-        result , result_col , result_diag , result_rev_diag = tictactoe.check_for_win(board=board)
-        board_game_is_on = tictactoe.check_for_board_game_on(result , result_col , result_diag , result_rev_diag)
+            player_side = player_two
 
 
-        print(result)
-        print(result_col)
-        print(result_diag)
-        print(result_rev_diag)
+        while True:
+            #change number to start with 1 not 0
+            player_input = input("Please enter your choice from 1 to 9 : ")
+            if tictactoe.validate_user_input(player_input=player_input):
+                break
+            else:
+                print("Invalid input , please try again")
 
-        if not board_game_is_on :
-            print("Game over!")
-            is_replay = input("Do you want to quit this game?")
+        player_input = tictactoe.player_input(player_inputs = player_input)
 
-            if is_replay.upper() == 'Y':
-                replay_on = False
-                board_game_is_on = False
+        check_the_board = tictactoe.check_whole_board(board=board , row=int(player_input[0]) ,column=int(player_input[1]))
+
+        if not check_the_board :
+            player_input = input("that place already be chosen. Please choose empty space : ")
+
+        print(check_the_board)
+
+        board = tictactoe.inserting_board(board_list=board, player_side=player_side , input = player_input)
+        display_board = tictactoe.display_board_after_insert(board_list=board)
+
+        print(display_board)
+
+        # check if turn count is over five and under nine then check
+        if turn_count >= 5:
+            result , result_col , result_diag , result_rev_diag = tictactoe.check_for_win(board=board)
+            board_game_is_on = tictactoe.check_for_board_game_on(result , result_col , result_diag , result_rev_diag)
+
+
+            print(result)
+            print(result_col)
+            print(result_diag)
+            print(result_rev_diag)
+
+            if not board_game_is_on :
+                print("Game over!")
+                is_replay = input("Do you want to quit this game?")
+
+                if is_replay.upper() == 'Y':
+                    replay_on = False
+                    board_game_is_on = False
 
 
 
-    #check if there are bingo
-    #inspecting board
+        #check if there are bingo
+        #inspecting board
 
-    turn_count += 1
+        turn_count += 1
 
 
 #Replay TicTacToe game
